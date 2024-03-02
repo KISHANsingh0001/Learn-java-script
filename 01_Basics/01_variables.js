272c9623c27c20a29bc90185a6aaf5594c127e84
KISHAN SINGH THAKUR <126978912+KISHANsingh0001@users.noreply.github.com>
2024-03-02 17:29:12 +0000

    Basics of js

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+// Declearing the variable in Js
+const accountId = 144553  // constant 
+//  variable
+let accountEmail = "kishansinghthakur27@gmail.com"
+var accountPassword = "12345";
+accountCity = "jabalpur";
+let accountState; // if we create a variable with no value so js will treat as a UNDIFINED
+/*
+prefer not to use var
+because of issue in block scope and functional block
+*/
+//accountId = 2;
+console.log(accountId); // printing single variable
+console.table({ accountId, accountEmail, accountPassword, accountCity, accountState }) // printing the variables in tabuler form
