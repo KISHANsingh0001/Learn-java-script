9aa2f5d4b224ee3f198f8059b5ce5199f1afeb21
KISHAN SINGH THAKUR <126978912+KISHANsingh0001@users.noreply.github.com>
2024-03-02 17:53:11 +0000

    Data types

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all Js code as newer version
+
+//alert(3+3  we are using Node js not browser
+
+console.log(3 + 3);   // code readability that should be high
+console.log("kishan");
+
+let name = "kishan"  // string data type
+let age = 18;
+let isLoggedIn = false;
+let state ;
+
+// number => 2^53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefinde => value is not assigned
+// symbol => unique
+
+// object 
+console.log(typeof null); // object will be print
+console.log(typeof undefined); // undefined
