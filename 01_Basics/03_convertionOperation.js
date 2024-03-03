c6eaf7b6ef955353c0e9477e576cd4d0859c3f0e
kishan singh thakur <kishasinghthakur27@gmail.com>
2024-03-03 23:37:54 +0530

    Conversion of Data types

--- 01_Basics/03_convertionOperation.js
+++ 01_Basics/03_convertionOperation.js
@@ -1,26 +1,61 @@
-let score = "33"; // this is the string type
-let scoree = "33aB";
-console.log(typeof(score)); // it gives the type string
+// let score = "33"; // this is the string type
+// let scoree = "33aB";
+// console.log(typeof(score)); // it gives the type string
 
-let valueInNumber = Number(score);
-let valueInNumbers = Number(scoree);
-console.log(typeof valueInNumber); // it gives the type number
-console.log(typeof valueInNumbers); // it gives the type number
-console.log(valueInNumbers); // it gives NaN (means Not a Number)
+// let valueInNumber = Number(score);
+// let valueInNumbers = Number(scoree);
+// console.log(typeof valueInNumber); // it gives the type number
+// console.log(typeof valueInNumbers); // it gives the type number
+// console.log(valueInNumbers); // it gives NaN (means Not a Number)
 
-// "33" => 33
-// "33abc" => NaN
-//  true => 1; false =>0
+// // "33" => 33
+// // "33abc" => NaN
+// //  true => 1; false =>0
 
-let isLoggedIn = "";
+// let isLoggedIn = "";
 
-let booleanIsloggedIn = Boolean(isLoggedIn);
-console.log(booleanIsloggedIn);
+// let booleanIsloggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsloggedIn);
 
-// 1 => true ; 0 => false
-// "" => false
-// "kishan" => true
+// // 1 => true ; 0 => false
+// // "" => false
+// // "kishan" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber);
-console.log(stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+
+// **********************Operations********************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue); it will give the negitive value
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " kishan"
+// let str3 = str1 + str2; // concatination of the string
+// console.log(str3);
+
+// console.log("1" + 2); // it will output 12
+// console.log("1" + 2 + 2); // it will output 122
+// console.log(2 + 2 + "1"); // it will output 41
+
+// console.log((3 + 4) * 5 % 3);
+// console.log(+true); // will give 1
+// console.log(+""); // will give 0
+
+// let num1 , num2 , num3
+// num1 = num2 = num3 = 2 + 2  // this is bed practice
+
+let gameCounter = 100;
+gameCounter++; // post increment
+++gameCounter; // pre increment
+console.log(gameCounter);
+
